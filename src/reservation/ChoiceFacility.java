66d8c943c7711ca57f82bd9653e5805af3122c23
b14ck9127 <gargoyle9127@yahoo.co.jp>
2017-02-08 15:30:29 +0900

    20170130 submit

---
+++ src/reservation/ChoiceFacility.java
@@ -0,0 +1,19 @@
+package reservation;
+
+import java.awt.Choice;
+
+public class ChoiceFacility extends Choice{
+	 public ChoiceFacility(){
+		 add("¬ƒz[ƒ‹");
+		 add("‘å‰ï‹cê‚P");
+		 add("‘å‰ï‹cê‚Q");
+		 add("¬‰ï‹cê‚P");
+		 add("¬‰ï‹cê‚Q");
+		 add("¬‰ï‹cê‚R");
+         add(" ¬‰ï‹cº4");
+         add(" ¬‰ï‹cº5");
+         add(" ¬‰ï‹cº6");
+
+	 }
+
+}
